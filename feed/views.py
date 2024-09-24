4a13e01cb3028ff1bf8c12a46da5a50f99a99664
Lacey W <lacey.m.washington@gmail.com>
2024-09-23 22:46:18 -0500

    added a way to add images to feed

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,8 @@
 from typing import Any
-from django.views.generic import TemplateView
+from django.http import HttpResponse
+from django.views.generic import TemplateView, DeleteView, FormView
 from .models import Post
+from .forms import PostForm
 
 class HomePageView(TemplateView):
     template_name ="home.html"
@@ -9,3 +11,20 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['post'] = Post.objects.all()
         return context
+    
+class PostDetailView(DeleteView):
+    template_name = "detail.html"
+    model = Post
+  
+  
+class AddPostView(FormView):
+   template_name = "new_post.html"
+   form_class =  PostForm
+   success_url = "/"
+   
+   def form_valid(self, form: Any) -> HttpResponse:
+       new_object = Post.objects.create(
+           text = form.cleaned_data['text'],
+           image = form.cleaned_data['image']
+       )
+       return super().form_valid(form)
